1841a41624ba7bd10816e9c0dd34e8d81e05168c
Will Nayes <wnayes@gmail.com>
2012-12-20 12:32:30 -0600

    Created some API method definitions.

--- api/index.php
+++ api/index.php
@@ -10,10 +10,37 @@ require 'Slim/Slim.php';
 
 $app = new \Slim\Slim();
 
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
-});
+$app->get('/events/:id', 'getEvent');
+$app->get('/events/search/:query', 'getEventsByLocation');
+$app->post('/events', 'createEvent');
 
 $app->run();
+
+function getEvent($id) {
+  // Get information about a particular event. Necessary?
+  // This may be useful for debugging but not used in practice.
+}
+
+function getEventsByLocation() {
+  $request = Slim::getInstance()->request();
+  $event = json_decode($request->getBody());
+  // Parse the long/lat and perform a SQL query.
+}
+
+function createEvent() {
+  $request = Slim::getInstance()->request();
+  $event = json_decode($request->getBody());
+  // Add the event information into the SQL Database
+}
+
+// Helper method for database connections.
+function getConnection() {
+  // The database credentials are kept out of revision control.
+  include("/location-outside-webroot/db_settings.php");
+  $dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  unset ($db_user, $db_pass);
+  return $dbh;
+}
 
 ?>
