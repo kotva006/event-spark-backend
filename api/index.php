9d11cc2229d08ee9eb1ac01a02c9fc21aaabc7e5
Will Nayes <wnayes@gmail.com>
2013-01-26 14:02:10 -0600

    Event search working by location. Fixed use of table variable from settings file. Settings file moved above webroot.

--- api/index.php
+++ api/index.php
@@ -4,15 +4,16 @@
 // See http://coenraets.org/blog/2011/12/restful-services-with-jquery-php-and-the-slim-framework/
 // for an idea of how to implement GET/POST/etc handlers.
 
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
+
 require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
 $app = new \Slim\Slim();
 
-
-
 $app->get('/events/:id', 'getEvent');
-$app->get('/events/search/:query', 'getEventsByLocation');
+$app->get('/events/search/', 'getEventsByLocation');
 $app->post('/events', 'createEvent');
 
 $app->run();
@@ -31,53 +32,56 @@ function getEvent($id) {
 //
 // Returns:
 //   {
-//    { event object }
-//    { event object }
-//    ...
+//     { event object }
+//     { event object }
+//     ...
 //   }
 function getEventsByLocation() {
   $request = \Slim\Slim::getInstance()->request();
-  $event = json_decode($request->getBody());
+
+  $latitude = $request->get('latitude');
+  $longitude = $request->get('longitude');
 
   // Simple error checking for valid inputs
-  if (IsNullOrEmptyString($event['longitude']) || IsNullOrEmptyString($event['latitude'])) {
-    echo '{"text":"invalid inputs for query"}';
+  if (isNullOrEmptyString($longitude) || isNullOrEmptyString($latitude)) {
+    echo '{"text": "invalid inputs for query"}';
     die;
   }
 
   // This is implementing a search range.  Will need to fine tune.
   // Changed to ~5 mile radius.
   $locationRadius = .074;
-  $latsma = $event['latitude'] - $locationRadius;
-  $latbig = $event['latitude'] + $locationRadius;
-  $lonsma = $event['longitude'] - $locationRadius;
-  $lonbig = $event['longitude'] + $locationRadius;
-  
-
-  // Creation the SQL query string.
-  $query = "SELECT * "
-           . "FROM $table "
-           . "WHERE longitude BETWEEN :lonsma AND :lonbig "
-           . "AND latitude BETWEEN :latsma AND :latbig";
-  
-  
+  $latsmall = $latitude - $locationRadius;
+  $latbig = $latitude + $locationRadius;
+  $lonsmall = $longitude - $locationRadius;
+  $lonbig = $longitude + $locationRadius;
 
   try {
     $dbx = getConnection();
+
+    // Creation the SQL query string.
+    $query = "SELECT * "
+           . "FROM " . $GLOBALS['table'] . " "
+           . "WHERE longitude BETWEEN :lonsmall AND :lonbig "
+           . "AND latitude BETWEEN :latsmall AND :latbig";
+
     $stmt = $dbx->prepare($query);
-    $stmt->bindParam("lonsma", $lonsma);
+    $stmt->bindParam("lonsmall", $lonsmall);
     $stmt->bindParam("lonbig", $lonbig);
-    $stmt->bindParam("latsma", $latsam);
+    $stmt->bindParam("latsmall", $latsmall);
     $stmt->bindParam("latbig", $latbig);
-    $stmt->bindParam("type", $type);
+    //$stmt->bindParam("type", $type);
+
     $stmt->execute();
+
     // Will need testing, but should give multiple objects in one.
     $events = $stmt->fetchAll(PDO::FETCH_OBJ);
+
     $dbx = NULL;
-    echo '{"event":' . json_encode($events) . '}';
+    echo '{"events":' . json_encode($events) . '}';
   }
   catch (PDOException $e) {
-    echo '{"text":"' . $e->getMessage() . '"}';
+    echo '{"text": "' . $e->getMessage() . '"}';
     $dbx = NULL;
     die;
   }
@@ -89,23 +93,22 @@ function createEvent() {
   $request = \Slim\Slim::getInstance()->request();
   $event = json_decode($request->getBody(),  true);
 
-  // Add the event information into the SQL Database
-  $query = "INSERT INTO events_2 (title, description, longitude, "
-         . "latitude, start_date, end_date) "
-         . "VALUES (:title, :description, :longitude, :latitude, "
-         . ":start_date, :end_date)";
-
   // Error checking for valid inputs
-  if (IsNullOrEmptyString($event['title']) ||
-      IsNullOrEmptyString($event['longitude']) ||
-      IsNullOrEmptyString($event['latitude']) ||
-      IsNullOrEmptyString($event['end_date'])) {
-    echo '{"text":"invalid inputs for event creation."}';
+  if (isNullOrEmptyString($event['title']) ||
+      isNullOrEmptyString($event['longitude']) ||
+      isNullOrEmptyString($event['latitude']) ||
+      isNullOrEmptyString($event['end_date'])) {
+    echo '{"text": "invalid inputs for event creation."}';
     die;
   }
 
   try {
     $dbx = getConnection();
+
+    // Add the event information into the SQL Database
+    $query = "INSERT INTO " . $GLOBALS['table'] . " (title, description, longitude, latitude, start_date, end_date) "
+           . "VALUES (:title, :description, :longitude, :latitude, :start_date, :end_date)";
+
     $state = $dbx->prepare($query);
     $state->bindParam("title", $event['title']);
     $state->bindParam("description", $event['description']);
@@ -115,21 +118,25 @@ function createEvent() {
     $state->bindParam("start_date", $event['start_date']);
     $state->bindParam("end_date", $event['end_date']);
     // Returns an object with bool:true.
-    echo '{"bool":"' . $state->execute() . '"}';
+    echo '{"bool": "' . $state->execute() . '"}';
     $dbx = NULL;
   }
   catch (PDOException $e) {
-    echo '{"text":"' . $e->getMessage() . '"}';
+    echo '{"text": "' . $e->getMessage() . '"}';
     $dbx = NULL;
     die;
   }
 }
 
 // Helper method for database connections.
-//Also include variable $table in settings.php
+// Also include variable $table in settings.php
 function getConnection() {
   // The database credentials are kept out of revision control.
-  include("/home/rkotval/settings/settings.php");
+  include("$_SERVER[DOCUMENT_ROOT]/../settings.php");
+
+  // Keep the table variable available globally.
+  $GLOBALS["table"] = $table;
+
   $dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   unset ($db_user, $db_pass);
@@ -137,7 +144,7 @@ function getConnection() {
 }
 
 // Validation helper (String is present and neither empty nor only white space)
-function IsNullOrEmptyString($field) {
+function isNullOrEmptyString($field) {
   return (!isset($field) || trim($field) === '');
 }
 
